7225f3072ec245c1daca9d4d560f9a8360f59892
Esteban Cara de Sexo <8595080+canstralian@users.noreply.github.com>
2025-02-24 05:15:06 +1000

    Update config.py

--- app/config.py
+++ app/config.py
@@ -2,4 +2,4 @@ import os
 
 class Config:
     DEBUG = os.getenv("FLASK_DEBUG", True)
-    SECRET_KEY = os.getenv("SECRET_KEY", "your_secret_key_here")
+    SECRET_KEY = os.getenv("SECRET_KEY", "default_secret_key")
